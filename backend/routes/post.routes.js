63845f58f0aeb64cc60b269bcafdb7cac4df014a
amansingh456 <amansinghrajawat690@gmail.com>
2023-08-22 21:35:46 +0530

    post fetch comments api done

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const { addingPost, gettingAllPost, getSinglePost } = require("../controllers/post.controllers")
+const { fetchComments, addComments } = require("../controllers/comment.controllers")
 
 
 const postRouter = express.Router()
 
 postRouter.post("/addpost", addingPost)
 postRouter.get("/allposts", gettingAllPost)
-postRouter.get("/post/:id", getSinglePost)
+postRouter.get("/:id", getSinglePost)
+postRouter.get("/comments/:id", fetchComments)
+postRouter.post("/addcomment/:id", addComments)
 
 module.exports={postRouter}
