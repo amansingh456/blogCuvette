e3a3e1049aaa692a57712af48cddfe49816a8ae7
amansingh456 <amansinghrajawat690@gmail.com>
2023-09-14 17:57:36 +0530

    database changed

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 // require("dotenv").config()
 
 const db = mysql.createConnection({
-  host: "mydb.cpdmi4ml6suu.eu-north-1.rds.amazonaws.com",
-  user: "admin",
-  password: "amansingh",
+  host: "localhost",
+  user: "root",
+  password: "aman",
   database: "cuvetteblog",
 });
 
