e5f5ce017395888c27eec1b51f81c324e2355fef
amansingh456 <amansinghrajawat690@gmail.com>
2023-09-14 20:05:22 +0530

    database changed..

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 // require("dotenv").config()
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "aman",
-  database: "cuvetteblog",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12646424",
+  password: "X6iTjmu4F8",
+  database: "sql12646424",
 });
 
 module.exports = db;
